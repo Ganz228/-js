3d01477015eb310a0a70e08dfbcd16d63dbb2d6f
Ganz228 <144236913+Ganz228@users.noreply.github.com>
2023-10-24 16:00:43 +0900

    лабараторная работа 2

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-'use strict'
+let x = 5;
+let y = 1;
+let d = -4;
+let c = -2;
 
-let x = Number(prompt("Введите x ="));
-let y = +prompt("Введите н =");
+let e;
 
-let a = (14 * Math.sin(x)+ Math.pow(y, 2))/(0.92 * Math.pow(Math.cos(x), 3))
+if(x * y > 3) {
 
-console.log(a);
+let maxEl = Math.pow(x, 3);
+if(Math.pow(x, 3) > Math.log(Math.pow(x * y, c * d))){
+    maxEl = Math.log(Math.pow(x, 3));
+}
+
+e = maxEl;
+}else if(0 <= x * y && x * y <= 3){
+   
+    let maxEl = d * y;
+    if(maxEl > c * x){
+        maxEl = c * x;
+    }
+    
+  
+    let minEl = y * 3;
+    if(minEl < x * 3){
+        minEl = x * 3;
+    }
+    
+    //Предположение
+    if(minEl < maxEl * 3){
+        minEl = maxEl * 3;
+    }
+
+    e = maxEl;
+}else if(x * y < 0){
+    e = Math.pow(2, c * d) - x;
+}
+console.log(e);
